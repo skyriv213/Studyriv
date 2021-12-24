d9574186559b32e1d32651732e305caa788434b5
skyriv <gn1007@dankook.ac.kr>
2021-12-25 01:54:17 +0900

    17강 너비 우선탐색 재작성

--- Major_all_in_one_C/Data structue_Algorithm/17강_그래프_너비 우선 탐색/main.c
+++ Major_all_in_one_C/Data structue_Algorithm/17강_그래프_너비 우선 탐색/main.c
@@ -49,3 +49,55 @@ void queuePush(Queue* queue, int index) {
 	queue->rear = node;
 	queue->count++
 }
+
+int queuePop(Queue* queue) {
+	if (queue->count == 0) {
+		printf("큐 언더플로우가 발생하였습니다.\n");
+		return -INF
+	}
+	Node* node = queue->front;
+	int index = node->index;
+	queue->front = node->next;
+	free(node);
+	queue->count--;
+	return index;
+}
+
+void bfs(int start) {
+	Queue  q;
+	q.front = q.rear = NULL;
+	q.count = 0;
+	queuePush(&q, start);
+	c[start] = 1;
+	while (q.count != 0) {
+		int x = queuePop(&q);
+		printf("%d ", x);
+		Node* cur = a[x]->next;
+		while (cur != NULL) {
+			int next = cur->index;
+			if (!c[next]) {
+				queuePush(&q, next);
+				c[next] = 1;
+			}
+			cur = cur->next;
+		}
+	}
+}
+
+int main(void) {
+	scanf("%d %d ", &n, &m);
+	a = (Node**)malloc(sizeof(Node*) * (MAX_SIZE));
+	for (int i = 1; i <= n; i++) {
+		a[i] = (Node*)malloc(sizeof(Node));
+		a[i]->next = NULL;
+	}
+	for (int i = 0; i < m; i++) {
+		int x, y;
+		scanf("%d %d ", &x, &y);
+		addFront(a[x], y);
+		addFront(a[y], x);
+	}
+	bfs(1);
+	system("pause");
+	return 0;
+}
