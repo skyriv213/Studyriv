fefa0db6eb11472c48b26a0e78c924d1784e15a7
skyriv <gn1007@dankook.ac.kr>
2022-01-03 01:03:09 +0900

    22.01.02기타부분 알고리즘 풀이

---
+++ Back_Solve/src/Etc/P11718_11719.java
@@ -0,0 +1,28 @@
+package Etc;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class P11718_11719 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(
+				new InputStreamReader(System.in));
+		BufferedWriter bw  = new BufferedWriter(
+				new OutputStreamWriter(System.out));
+		
+		String s;
+		while((s = br.readLine())!=null) {
+			bw.write(s+"\n");
+			bw.flush();
+
+		}
+		bw.close();
+
+
+	}
+}
