4e0917ba0b28cb2517d98002b173a1f0ceca536b
skyriv <gn1007@dankook.ac.kr>
2022-01-06 11:45:06 +0900

    22.01.06 최소공배수 solve

---
+++ Back_Solve/src/Etc/P13241.java
@@ -0,0 +1,28 @@
+package Etc;
+
+import java.util.Scanner; 
+
+public class P13241 {
+	static long GCD(long a, long b) {
+		long r = a%b;
+		if(r==0) {
+			return b;
+		}
+		else {
+			return GCD(b,r);
+		}
+	}
+	public static void main(String [] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for(int i = 0 ; i < n;i++) {
+			long a = sc.nextLong();
+			long b = sc.nextLong();
+			long x = GCD(a,b);
+			
+			System.out.println((long)a*b/x);
+			
+		}
+	}
+}
+
