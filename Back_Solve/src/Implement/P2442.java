eb48e3a9829b642b887f9177d80936e2f7cf2296
skyriv <gn1007@dankook.ac.kr>
2022-01-13 22:59:58 +0900

    22.01.13 구현/ 그리디 문제풀이

--- Back_Solve/src/Implement/P2442.java
+++ Back_Solve/src/Implement/P2442.java
@@ -10,21 +10,16 @@ public class P2442 {
 		
 		int n = sc.nextInt();
 		
-		for(int i  = 0 ;i< n;i++) {
-			for(int j = 0 ;j<n/2;j++) {
+		for(int i =1;i<=n;i++) {
+			for(int j =0;j<n-i;j++) {
 				System.out.print(" ");
 			}
-			for(int k = i
-					;k<n;k++) {
+			for(int j = 0;j<i*2-1;j++) {
 				System.out.print("*");
-
-
-			}
-			for(int j = 0 ;j<n/2;j++) {
-				System.out.print(" ");
 			}
 			System.out.println();
 		}
+		
 	}
 
 }
