78bff3805cefaedc8dd1479a71a14caf7831fb8a
skyriv <gn1007@dankook.ac.kr>
2022-01-16 23:39:06 +0900

    22.01.16 DFS 문제풀이

---
+++ Back_Solve/src/DFS_BFS/P11724.java
@@ -0,0 +1,59 @@
+package DFS_BFS;
+
+import java.util.Scanner;
+
+public class P11724 {
+	
+	static int [][] graph;
+	static boolean [] check;
+	
+	
+	// DFS 메소드
+	static void DFS(int num) {
+		// check[num]이 true일시 그냥 종료 및 반환
+		if(check[num]) {
+			return;
+		}
+		
+		// true로 값 지정 및 반복문 선언
+		check[num]=true;
+		for(int i = 1;i<graph.length;i++) {
+			if(graph[num][i]==1) {
+				DFS(i);
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt(); // 정점의 갯수
+		int m = sc.nextInt(); // 간선의 갯수
+	
+		// 정점의 인덱스를 편하게 하기 위해 1을 더해서 계산
+		graph = new int [n+1][n+1];
+		check = new boolean [n+1];
+		
+		// 인접행렬 그래프 입력 부분
+		for(int i = 1 ; i <=m;i++ ) {
+			int x =sc.nextInt();
+			int y =sc.nextInt();
+			graph[x][y]=1;
+			graph[y][x]=1;
+		}
+		
+		// 요소의 갯수 체크 변수 및 for 반복문 선언
+		int count  =0 ;
+		for(int i = 1; i <=n;i++) {
+			// i가 진행이 되면서 연결된 부분은 check 배열에서 이미 true로 걸러진다. 
+			// 그러므로 check[]에서 true가 아닌 부분의 경우
+			if(check[i]!=true) {
+			DFS(i);
+			count++;
+			}
+		}
+		// 갯수인 count를 출력
+		System.out.println(count);
+	}
+
+}
--- Back_Solve/src/DFS_BFS/P5567.java
+++ Back_Solve/src/DFS_BFS/P5567.java
@@ -1,14 +1,32 @@
 package DFS_BFS;
 
-import java.util.Arrays;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class P5567 {
 	
-	static boolean check(int [][]arr
-		return false;) {
-		
-	}
+	static int [][] graph;
+	static boolean [] check;
+	static int count ; // 결혼식에 초대하는 동기의 수
+	
+	
+	// DFS로 접근
+	static void DFS(int num,int depth) {
+		// depth가 2와 같아지면 종료
+		if(depth ==2) {
+			return;
+		}
+		// 1부터 그래프의 크기까지 반복문 실행
+		for(int i = 1 ; i < graph.length;i++) {
+				// 그래프 상에서 서로 연관이 되어 1이 체크가 되어있으면 if문 실행
+				if(graph[num][i]==1) {
+					// 해당 숫자의 boolean 배열을 ture로 변경 후 DFS의 depth를 1 더한후 재귀
+					check[i]= true;
+					DFS(i,depth+1);
+
+				}
+			}
+		}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -16,29 +34,27 @@ public class P5567 {
 		int n= sc.nextInt(); // 동기의 수
 		int m =sc.nextInt(); // 리스트의 길이
 		
-		int [][] graph = new int [n+1][n+1];
+		graph = new int [n+1][n+1];
 		
-		for(int i = 1; i <=n;i++) {
-			Arrays.fill(graph[i],0);
-		}
-		
-		boolean [] tf = new boolean [n+1];
-	
-		
+		// BFS or DFS를 진행하면서 회전을 넣을 boolean 배열의 크기 정해주기 
+		check = new boolean [n+1];
+				
 		for(int i = 1 ; i <=m ;i++) {
-			
 			int x = sc.nextInt();
 			int y = sc.nextInt();
 			graph [x][y] = 1;
 			graph [y][x] = 1;
 			
 		}
-		for(int i = 1 ; i < graph.length;i++) {
-			for(int  j = 1 ; j <graph[i].length;j++) {
-				System.out.print(graph[i][j] + " ");
+		count  =0;
+		DFS(1,0); // 상근이의 학번이 1이기에 1부터 시작
+		for(int i = 2 ; i <=n;i++) {
+			if(check[i]==true) {
+				count++;
 			}
-			System.out.println();
 		}
+		// 본인의 값일 경우 true가 포함이 되니 1이 아닌 2부터 카운팅
+		System.out.println(count);
 	}
 		
 	}
