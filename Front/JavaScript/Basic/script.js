442db92a4977e2b6b4723a64cfd88ecaae508dcc
skyriv <gn1007@dankook.ac.kr>
2022-01-21 23:41:34 +0900

    JavaScript Basic 22.01.21

---
+++ Front/JavaScript/Basic/script.js
@@ -0,0 +1,19 @@
+alert("hi");
+// 상수 바뀌지 않는 값
+const x = 5;
+const y = 2;
+console.log(x+y);
+
+
+const player = {
+    name : "kim",
+    age : 26,
+    point : 10,
+    fat : false
+}
+console.log(player);
+console.log(player.name);
+console.log(player.fat);
+console.log(player.age);
+console.log(player.point);
+
