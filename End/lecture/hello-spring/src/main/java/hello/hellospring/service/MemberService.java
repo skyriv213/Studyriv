45a0cbcef96f2d79bc1cc51c3ab6e029c44f628d
skyriv <gn1007@dankook.ac.kr>
2022-02-10 19:17:41 +0900

    Spring init - customer service develope

---
+++ End/lecture/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,44 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+
+    /*
+    회원가입
+     */
+    public Long join(Member member){
+        // 같은 이름이 있는 중복 회원 x
+        validateDuplicateMember(member); // 중복회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+    // 메소드 추출하기 shift + alt +m
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m ->{
+                    throw  new IllegalStateException("이미 존재하는 회원입니다");
+                });
+    }
+
+    /*
+    전체 회원 조회
+     */
+    public List<Member> findMembers(){
+        // 네이밍의 경우 각 상황에 맞게 구성을 하자
+        return memberRepository.findALl();
+    }
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById();
+    }
+
+
+}
