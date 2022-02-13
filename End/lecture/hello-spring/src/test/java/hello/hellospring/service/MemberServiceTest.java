59443f1fab525a21b61456669df6657b6cb5b87f
skyriv <gn1007@dankook.ac.kr>
2022-02-14 01:38:14 +0900

    Component_Scan & Auto_wired

--- End/lecture/hello-spring/src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ End/lecture/hello-spring/src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -3,11 +3,8 @@ package hello.hellospring.service;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemoryMemberRepository;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
