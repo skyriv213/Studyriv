e0fc2aded27b9bd502224b225b72dd1411100ea2
skyriv <gn1007@dankook.ac.kr>
2022-02-14 23:19:16 +0900

    누락내용 추가

--- Back_Solve/src/Dynamic_Programming/P2133.java
+++ Back_Solve/src/Dynamic_Programming/P2133.java
@@ -1,9 +1,31 @@
 package Dynamic_Programming;
 
+import java.util.Scanner;
+
 public class P2133 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] dp = new int[n + 1];
+		int answer = 0;
+		
+		if (n % 2 == 1) {
+			answer = 0;
+		} else {
+			dp[0] = 1;
+			dp[2] = 3;
+			for (int i = 4; i <= n; i += 2) {
+				dp[i] = dp[i - 2] * dp[2];
+				for (int j = i - 4; j >= 0; j -= 2) {
+					dp[i] += dp[j] * 2;
+				}
+			}
+			answer = dp[n];
+		}
+		System.out.println(answer);
+		sc.close();
 
 	}
 
