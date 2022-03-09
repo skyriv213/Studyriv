e4c43df2a48551c7ca8eab820369eebfe5430330
skyriv <gn1007@dankook.ac.kr>
2022-03-09 10:03:03 +0900

    Spring config

--- End/lecture/core/src/main/java/hello/core/AppConfig.java
+++ End/lecture/core/src/main/java/hello/core/AppConfig.java
@@ -7,22 +7,28 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(MemberRepository());
     }
-
-    private MemoryMemberRepository MemberRepository() {
+    @Bean
+    public MemoryMemberRepository MemberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(MemberRepository(), DiscountPolicy());
     }
 
-    private RateDiscountPolicy DiscountPolicy() {
+    @Bean
+    public RateDiscountPolicy DiscountPolicy() {
         return new RateDiscountPolicy();
     }
 
