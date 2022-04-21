c9ccec9222e7754f9bbff83a3bcb5558295cceb7
skyriv <gn1007@dankook.ac.kr>
2022-04-21 09:07:13 +0900

    mvc - 타임리프, HTML, 상품상세

---
+++ End/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -0,0 +1,52 @@
+package hello.itemservice.web.basic;
+
+
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor  // final이 붙은 요소를 가지고 생성자를 만들어준다
+public class BasicItemController {
+
+    private final ItemRepository itemRepository;
+
+    //    @Autowired // 스프링에서 생성자가 하나 있을경우 @Autowired 어노테이션을 생략할 수 있다.
+//    public BasicItemController(ItemRepository itemRepository) {
+//        this.itemRepository = itemRepository;
+//    }
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "basic/items";
+    }
+
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+
+    /***
+     * 테스트용 데이터 추가
+     */
+    @PostConstruct
+    public void init() {
+
+        itemRepository.save(new Item("itemA", 10000, 10));
+        itemRepository.save(new Item("itemB", 20000, 5));
+    }
+}
