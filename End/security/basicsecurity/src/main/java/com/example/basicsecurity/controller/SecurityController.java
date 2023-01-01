a1c348b68767e5ff0ede22a6b356d00baa4ef3e6
skyriv213 <gn1007@dankook.ac.kr>
2023-01-01 23:10:14 +0900

    form Login 인증

--- End/security/basicsecurity/src/main/java/com/example/basicsecurity/controller/SecurityController.java
+++ End/security/basicsecurity/src/main/java/com/example/basicsecurity/controller/SecurityController.java
@@ -7,7 +7,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class SecurityController {
 
     @GetMapping("/")
-    public String index(){
+    public String index() {
         return "home";
     }
+
+    @GetMapping("loginPage")
+    public String loginPage(){
+        return "loginPage";
+    }
+
 }
