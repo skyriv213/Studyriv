59a462a582a83567a4c19784e17708a1f0836916
skyriv213 <gn1007@dankook.ac.kr>
2023-06-02 14:50:34 +0900

    <kafka> -  consumer basic code

---
+++ End/kafka/lecture/kafka-biginners-course/.idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,5 @@
+
+I
+baisic-kafka/build.gradle,d\a\daa1f9ced5719845f6b8de6c9de28ce00124e8bd
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
---
+++ End/kafka/lecture/kafka-biginners-course/kafka-consumer-opensearch/src/main/java/io/conduktor/demos/kafka/opensearch/OpenSearchConsumer.java
@@ -0,0 +1,9 @@
+package io.conduktor.demos.kafka.opensearch;
+
+public class OpenSearchConsumer {
+
+    public static void main(String[] args) {
+        
+    }
+
+}
