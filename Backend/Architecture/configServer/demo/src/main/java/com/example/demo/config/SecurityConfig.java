4b8230ade24b8b45bdc4ea1fa718e9a1a22f755e
김민수 <kimminsoo213@dankoook.ac.kr>
2025-05-01 21:43:04 +0900

    study spring config server - use spring config server, security, lombok - use a rsa ssh key by ssh-keygen -m PEM rsa -b 4096 -C "skyriv213" and not use password - if i enter localhost:9000/ms0/dev, i can my test config application.yml

---
+++ Backend/Architecture/configServer/demo/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.example.demo.config;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain SecurityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable());
+        http.authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest().authenticated());
+        http.httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.builder().username("admin").password(bCryptPasswordEncoder().encode("1234")).roles("ADMIN").build();
+        return  new InMemoryUserDetailsManager(user);
+    }
+}
