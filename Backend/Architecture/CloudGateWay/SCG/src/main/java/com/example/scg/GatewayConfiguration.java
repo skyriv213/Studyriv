8a696f581a33c54d0bb29ce887cd62780511f3ce
김민수 <kimminsoo213@dankoook.ac.kr>
2025-06-04 23:31:25 +0900

    MSA - Spring Cloud Gateway Global, Local Filter example

--- Backend/Architecture/CloudGateWay/SCG/src/main/java/com/example/scg/GatewayConfiguration.java
+++ Backend/Architecture/CloudGateWay/SCG/src/main/java/com/example/scg/GatewayConfiguration.java
@@ -8,29 +8,27 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 @Configuration
 public class GatewayConfiguration {
 
+    private final L1Filter l1Filter;
+
+    public GatewayConfiguration(L1Filter l1Filter) {
+        this.l1Filter = l1Filter;
+    }
+
     @Value("${service.ms1}")
-    private String ms1Uri;  // URI로 바로 받으셔도 되지만, String으로 받은 뒤 URI.create(…) 해도 무방합니다.
-//    @Value("${service.ms2}")
-//    private String ms2Uri;
+    private String ms1Uri;
 
     @Bean
     public RouteLocator routeLocator(RouteLocatorBuilder builder) {
-        return
-                builder.routes()
-                        .route("ms1", r -> r.path("/ms1/**")
-                                .filters(f -> f.stripPrefix(1))  // <- 이 줄 추가
-                                .uri("lb://MS1"))
-
-
-//                        .route("ms2", r -> r.path("/ms2/**")
-//                                .filters(f -> f.stripPrefix(1))  // <- 이 줄 추가
-//                                .uri(ms2Uri))
-                        .build();
-
+        return builder.routes()
+                .route("ms1", r -> r.path("/ms1/**")
+                        .filters(f -> f
+                                .stripPrefix(1)
+                                .filter(l1Filter.apply(new L1Filter.Config(true, true)))
+                        )
+                        .uri("lb://MS1"))
+                .build();
     }
-
 }
